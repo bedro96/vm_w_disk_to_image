65f45dae9e46b9313fe5af5187965ff97729641c
bedro96@naver.com <bedro96@naver.com>
2019-12-18 14:53:35 +0000

    update

--- files/datadisk_setup.sh
+++ files/datadisk_setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt dist-upgrade -y
-
 # fdisk /dev/sdc
 sudo sh -c 'echo n\np\n\n\n\nw\n | fdisk /dev/sdc'
 # mkfs
@@ -23,17 +20,3 @@ inputstring=UUID=$sdc_uuid /logs ext4 defaults 1 2
 echo $inputstring | sudo tee -a /etc/fstab
 sudo umount /logs
 sudo mount /logs
-
-# Disabling cloud-init
-echo disable cloud-init
-sudo touch /etc/cloud/cloud-init.disabled
-sudo sed -i '/azure_resource/d' /etc/fstab
-echo comment out source VM config
-sudo sed -i -e 's/match:/#match:/g' /etc/netplan/50-cloud-init.yaml
-sudo sed -i -e 's/macaddress:/#macaddress:/g' /etc/netplan/50-cloud-init.yaml
-sudo sed -i -e 's/set-name:/#set-name:/g' /etc/netplan/50-cloud-init.yaml
-echo update the netplan config
-echo note you would need to restart the systemd.networkd.service to apply it
-echo this relies on a new VM starting the network with this config
-sudo netplan generate
-sudo netplan apply
